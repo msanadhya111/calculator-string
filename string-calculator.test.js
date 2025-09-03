53a9cffb77b4f24ce84fa03145116af0764d8b2e
Milan Sanadhya <milansanadhya1996@gmail.com>
2025-09-03 11:48:08 +0530

    feat: add support for custom delimiter

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -22,4 +22,8 @@ describe("string-calculator", () => {
     const number = add("1\n2,3");
     expect(number).toBe(6);
   });
+  test("support for custom delimiter", () => {
+    const number = add("//;\n1;2");
+    expect(number).toBe(3);
+  });
 });
